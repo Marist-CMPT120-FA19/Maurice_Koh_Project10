cb9087aa006af1253723f22cf1b9dfd457e77e42
MauriceKoh <54858943+MauriceKoh@users.noreply.github.com>
2019-11-12 18:05:37 -0500

    Create project10.py

---
+++ project10.py
@@ -0,0 +1,36 @@
+#Maurice Koh
+#zhi.koh1@marist.edu
+#Program that turns a curse sentence into a clean one
+
+
+def main():
+    #a = File that contains the censored words
+    #b = File that contains the words to censor
+    a = "Cursewords.txt"
+    b = "Sentence.txt"
+    #a = input("Enter the name of the censored words file: ")
+    #b = input("Enter the name of the file to censor: ")
+    
+    #read file
+    File = open(a ,"r")
+    Censor = open(b ,"r")
+
+    #split words
+    Words = File.read().split(" ")
+    words = Censor.read().split(" ")
+
+    #cycle to to read every word in the sentence and change it to *
+    for i in Words:
+        for j in range(0 , len(words)):
+            if i == words[j]:
+                words[j] = "*" * len(i)       
+        Censor = open(b , "w")
+    #to make sure it does not delete the whole sentence
+    for i in words:
+        Censor.write(i)
+        Censor.write(" ")
+        
+    Censor.close()
+    
+main()
+
